a749bb35168c584b2f0fa3fd64eac3a9b54a2e28
xBeam <rus.lytvynenko@gmail.com>
2025-03-12 18:07:29 +0100

    Initial commit
    Created model of book and library repository

---
+++ Library-Management/Library-Management/Interfaces/ILibraryRepository.cs
@@ -0,0 +1,15 @@
+﻿using Library_Management.Models;
+
+namespace Library_Management.Interfaces
+{
+    public interface ILibraryRepository
+    {
+        void AddBook(Book book);
+        void RemoveBook(string id);
+        List<Book> SearchBooks(string query);
+        List<Book> GetAvailableBooks();
+        void BorrowBook(string id);
+        void ReturnBook(string id);
+        void SaveChanges();
+    }
+}
---
+++ Library-Management/Library-Management/LibraryRepository.cs
@@ -0,0 +1,48 @@
+﻿using Library_Management.Interfaces;
+using Library_Management.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_Management
+{
+    public class LibraryRepository : ILibraryRepository
+    {
+        public void AddBook(Book book)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void BorrowBook(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Book> GetAvailableBooks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveBook(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ReturnBook(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Book> SearchBooks(string query)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
